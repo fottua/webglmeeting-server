4b5e44f0860b7d89d828cf7265c706be05f453b3
fottua <fott.ua@gmail.com>
2016-09-15 19:40:45 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,58 +1,15 @@
-var app = require('express').createServer();
-app.listen(4000);
-
-var webRTC = require('webrtc.io').listen(app);
-
-app.get('/examples/index-chat.html', function(req, res) {
-	res.sendfile(__dirname + '/examples/index-chat.html');
-});
-app.get('/examples/index-test.html', function(req, res) {
-	res.sendfile(__dirname + '/examples/index-test.html');
-});
-app.get('/examples/style.css', function(req, res) {
-	res.sendfile(__dirname + '/examples/style.css');
-});
-app.get('/examples/webrtc.io.js', function(req, res) {
-	res.sendfile(__dirname + '/node_modules/webrtc.io/node_modules/webrtc.io-client/lib/webrtc.io.js');
-});
+var http = require('http');
+var express = require('express');
+var app = express();
 
 
-webRTC.rtc.on('connect', function(rtc) {
-	//Client connected
-	console.log('client connected')
-});
 
-webRTC.rtc.on('send answer', function(rtc) {
- 	//answer sent
-});
 
-webRTC.rtc.on('disconnect', function(rtc) {
-	console.log('client disconnected')
-	//Client disconnect 
-});
-
-webRTC.rtc.on('chat_msg', function(data, socket){
-  var roomList = webRTC.rtc.rooms[data.room] || [];
-
-  for (var i = 0; i < roomList.length; i++) {
-    var socketId = roomList[i];
-
-    if (socketId !== socket.id) {
-      var soc = webRTC.rtc.getSocket(socketId);
-
-      if (soc) {
-        soc.send(JSON.stringify({
-          "eventName": "receive_chat_msg",
-          "data": {
-            "messages": data.messages,
-            "color": data.color
-          }
-        }), function(error) {
-          if (error) {
-            console.log(error);
-          }
-        });
-      }
-    }
-  }
+var request = http.get("http://stats.zello.com/channels-suggest/ru/", function(response) {
+  app.get('/', function (req, res) {
+    res.send(request);
+  });
+  app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+  });
 });
